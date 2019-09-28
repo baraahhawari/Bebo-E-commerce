24376fca91e57ab0f4a323fb9af44b62940e1281
Bara'ah Hawari <51928607+baraahhawari@users.noreply.github.com>
2019-09-28 11:34:42 +0300

    Update karorajs.js

--- js/karorajs.js
+++ js/karorajs.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+
   $('.scroll-to-top').click(function() {
     $('html, body').animate({scrollTop: 0}, 800);
   return false;
@@ -8,6 +10,7 @@ $('.popup-close').on('click', function(){
 			
 			$('#subscribe-popup').fadeOut('slow');
 		});
+
 $('.subscribe-btn').on('click',function() {
 			$('#subscribe-popup').fadeOut('slow');
 		});
@@ -27,11 +30,19 @@ $(".expand_action").on('click', function(){
 
 
 
+$('#hide-popup').on('click', function() {
+        $('#subscribe-popup').hide();
+        $(this).remove();
+    });
+
+
+
+$('.search-hide').hide();
+$('.search-btn').on('click', function(e){
+			e.preventDefault();
+$(this).toggleClass('search-open');
+$('.search-hide').fadeToggle('slow');
+		});
 
 
 });
-
-
-		
-		
-	
